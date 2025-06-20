03c67e1c62808c5dc92cef3614358661ed4ad1af
Kamlesh Parihar <113664592+pariharkamal9829@users.noreply.github.com>
2025-06-20 14:34:32 +0530

    task  2to uploaded

---
+++ terraform-ec2-nginx/main.tf
@@ -0,0 +1,51 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_security_group" "allow_ssh_http" {
+  name        = "allow_ssh_http"
+  description = "Allow SSH and HTTP traffic"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "nginx_server" {
+  ami           = "ami-0c02fb55956c7d316"
+  instance_type = "t2.micro"
+  security_groups = [aws_security_group.allow_ssh_http.name]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              yum update -y
+              amazon-linux-extras install nginx1 -y
+              systemctl start nginx
+              systemctl enable nginx
+              EOF
+
+  tags = {
+    Name = "Terraform-Nginx-Server"
+  }
+}
+
+output "public_ip" {
+  value = aws_instance.nginx_server.public_ip
+}
